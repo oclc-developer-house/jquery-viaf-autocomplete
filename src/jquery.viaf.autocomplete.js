b4af573b88f03f2c9c7adbeee56e2c06e3c12196
Bobbi_SMR <bobbi_fox@harvard.edu>
2014-02-06 15:22:40 -0500

    handling the nametype now; temporary assignment of nametypes in the code in order to test.

--- src/jquery.viaf.autocomplete.js
+++ src/jquery.viaf.autocomplete.js
@@ -132,12 +132,30 @@
 				success: function(data) {
 					var ct = 0; 
 					response( $.map( data.result, function(item) {
+						// CUT THIS OUT WHEN WE GET THE NEW ENDPOINT
+						if (ct % 4 ===  0) {
+							item.nametype = "personal";
+						}
+						else if (ct % 4 === 1) {
+							item.nametype="work";
+						}
+						else if (ct % 4 === 2) {
+							item.nametype="organization";
+						}
+						else {
+							item.nametype="geographic";
+						}
+						// END OF CUT OUT
 						var retLbl = item.term;
+						if (item.nametype) {
+							retLbl = retLbl + " [" + item.nametype + "]";
+						}
 						ct++;
 						return {
 							label: retLbl,
-							value: retLbl,
-							id: item.viafid
+							value: item.term,
+							id: item.viafid,
+							nametype: item.nametype
 						}
 					}));
 					if (o.oncount && $.isFunction(o.oncount)) {
